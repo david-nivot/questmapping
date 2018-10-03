97349211d68985d7698fbc2c5ab3220da746f26e
David NIVOT <d.nivot@polymagine.com>
2018-10-03 17:47:40 +0200

    Added usergroup loader

---
+++ loaders/user_group.js
@@ -0,0 +1,31 @@
+const request = require('request');
+const csv = require('csvtojson');
+const sequelize_fixtures = require('sequelize-fixtures');
+const models = require('../models');
+
+async function cleanFromDB() {
+    await models.UserGroup.destroy({where: {}});
+}
+
+function processRow( row ) {
+    sequelize_fixtures.loadFixture({ model: 'UserGroup', data: row }, models);
+}
+
+request.get(process.argv[2], function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        csv({ignoreEmpty:true})
+        .fromString(body)
+        .then((json)=>{
+            cleanFromDB()
+            .then(function() {
+                json.forEach(processRow);
+            })
+            .catch(function(error) {
+                console.error(error);
+            });
+        })
+    }
+    else {
+        console.error("Failed to load user groups.");
+    }
+});
